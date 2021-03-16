182b801731aef6339493b3f38796c00190b13724
Arthur Sandro <arthurcs426@gmail.com>
2021-03-15 17:30:04 -0700

    adding a test to use react transition group

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   }
 
   showModal = () => {
@@ -19,10 +20,22 @@ class App extends Component {
   }
 
   render() {
-    const { modalIsOpen } = this.state;
+    const { modalIsOpen, showBlock } = this.state;
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button onClick={() => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>Toggle</button>
+        {
+          showBlock ? (
+            <div 
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+              }}>
+            </div>
+          ) : null
+        }
         {modalIsOpen ? <Modal show={modalIsOpen} closed={this.closeModal} /> : null}
         {modalIsOpen ? <Backdrop show={modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
